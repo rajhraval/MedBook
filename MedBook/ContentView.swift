db1f269f05ebc560a01309de4cdcd4d17eeed93b
Raj <rajraval37@gmail.com>
2023-10-10 18:16:00 +0530

    Intial Boilerplate Code

---
+++ MedBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedBook
+//
+//  Created by Raj Raval on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedBook/MedBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBookApp.swift
+//  MedBook
+//
+//  Created by Raj Raval on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MedBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
